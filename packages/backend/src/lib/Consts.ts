9f13b2e8a51364b4596a249a1330014ceb4f9ace
Chris Koeth <vdawg@tuta.io>
2022-12-04 19:29:35 +0200

    improve SQL query typing

--- packages/backend/src/lib/Consts.ts
+++ packages/backend/src/lib/Consts.ts
@@ -3,19 +3,26 @@ import type { ISQLDatabaseItemString } from "@/types/Types"
 /**
  * Making the SQL strings typesafer
  */
-export const SQL_STRINGS: { [item in ISQLDatabaseItemString]?: item } = {
+export const SQL_STRINGS = {
   "Album.name": "Album.name",
   "Artist.name": "Artist.name",
   "Cover.filepath": "Cover.filepath",
-  "Track.album": "Track.album",
-  "Track.cover": "Track.cover",
-  "Track.title": "Track.title",
-  "Track.artist": "Track.artist",
+  "Cover.md5": "Cover.md5",
+  "PlaylistItem.trackID": "PlaylistItem.trackID",
+  "PlaylistItem.playlistID": "PlaylistItem.playlistID",
   "Track.*": "Track.*",
+  "Track.album": "Track.album",
+  "Track.artist": "Track.artist",
+  "Track.cover": "Track.cover",
+  "Track.filepath": "Track.filepath",
+  "Track.id": "Track.id",
+  "Track.title": "Track.title",
+
   Album: "Album",
   Artist: "Artist",
   Cover: "Cover",
+  PlaylistItem: "PlaylistItem",
   Track: "Track",
   filepath: "filepath",
   name: "name",
-}
+} satisfies { [item in ISQLDatabaseItemString]?: item }
